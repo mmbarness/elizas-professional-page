7278f450e9ed264356737e1ed2b2928992291cd7
mmbarness <011mbarnes@gmail.com>
2022-02-10 15:44:39 +0100

    music video semi-complete

--- src/features/07-musicVideos/MusicVideos.tsx
+++ src/features/07-musicVideos/MusicVideos.tsx
@@ -1,12 +1,39 @@
 import { useGetMusicVideosQuery } from "../shared/sanityAPI"
+import {Suspense, useCallback, useState, useEffect} from "react"
+import ReactPlayer from "react-player/lazy"
 
 export const MusicVideos = () => {
 
     const { isLoading, error, data } = useGetMusicVideosQuery()
     console.log(data)
+
+    const renderMusicVideos = useCallback(
+        () => {
+            if (data) {
+                return data.map((video, i) => (
+                    <Suspense fallback={<div></div>}>
+                        <ReactPlayer
+                            url={video.link}
+                            width="90%"
+                            height= "100%"
+                            style={{
+                                // margin: ".5rem",
+                            }}
+                            light={true}
+                            controls={true}
+                            className={`${i % 2 === 0 ? "left music-video" : "right music-video"}`}
+                            // origin={window.location.origin}
+                        />
+                    </Suspense>
+                ))
+            }
+        },
+        [data]
+    )
+
     return (
-        <div>
-            <h1>Music Videos</h1>
+        <div id="music-video-container">
+            {renderMusicVideos()}
         </div>
     )
 }

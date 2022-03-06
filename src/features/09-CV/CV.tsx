a384c385a8eb25b893562aa3837187ba83804244
mmbarness <011mbarnes@gmail.com>
2022-03-06 16:52:43 -0500

    correct dynamic resume canvas resizing

--- src/features/09-CV/CV.tsx
+++ src/features/09-CV/CV.tsx
@@ -4,10 +4,33 @@ import { pdfjs, Document, Page } from "react-pdf";
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import { useGetResumeQuery } from "../shared/sanityAPI";
 import rightArrow from '../../assets/right-arrow.svg';
+import { returnLayout } from "../shared/quickies";
 
 export const CV = () => {
 
     const { isLoading, error, data } = useGetResumeQuery()
+    
+    const dimensionDict = {
+        'portrait': {
+            'width': (width:number) => width - 225,
+            'height': (height:number) => height - 225
+        },
+        'landscape': {
+            'width': (width:number) => width - 225,
+            'height': (height:number) => height - 225
+        }
+    }
+
+    const [layout, setLayout] = useState<'portrait'|'landscape'>(returnLayout())
+
+    const [windowDimensions, setWindowDimensions] = useState({
+        'width': dimensionDict[layout].width(window.innerWidth), 
+        'height': dimensionDict[layout].height(window.innerHeight), 
+        'lesserValue': Math.min(
+            dimensionDict[layout].width(window.innerWidth), 
+            dimensionDict[layout].height(window.innerHeight)
+        )
+    })
 
     const [pageNumber, setPageNumber] = useState(1);
 
@@ -15,30 +38,21 @@ export const CV = () => {
 
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-    const getWindowDimension = (whichSide: string) => ( whichSide === 'width' ? window.innerWidth-50 : window.innerHeight-50)
-    
-    const [windowDimensions, setWindowDimensions] = useState({'width': getWindowDimension('width'), 'height': getWindowDimension('height'), 'lesserValue': getWindowDimension('width')})
-    
-    const handleResize = () => {
-        setWindowDimensions({'width': getWindowDimension('width'), 'height': getWindowDimension('height'), 'lesserValue': getWindowDimension('width')})
+    const handleResponsiveCanvas = () => {
+        const {width: widthFn, height: heightFn} = dimensionDict[layout]
+        const [width, height] = [widthFn(window.innerWidth), heightFn(window.innerHeight)]
+        const lesserValue = Math.min(width, height)
+        setWindowDimensions({width, height, lesserValue})
     }
 
     useEffect(() => {
-        handleResponsiveCanvas()
-    }, [setWindowDimensions]);
+        setLayout(returnLayout())
+    },[window.innerWidth, window.innerHeight])
 
-    window.addEventListener('resize', handleResize);
-
-    const handleResponsiveCanvas = () => {
-        const [ width, height ] = [ windowDimensions.width, windowDimensions.height ];
-        const lesserValue = width < height ? width : height;
-        const layout = (width / height) > 1 ? "landscape" : "portrait";
-        if (layout === "portrait") {
-            setWindowDimensions({'width': width - 20, 'height': height, 'lesserValue': lesserValue})
-        } else {
-            setWindowDimensions({'width': width - 200, 'height': height - 200, 'lesserValue': lesserValue - 200})
-        }
-    }
+    useEffect(() => {
+        window.addEventListener('resize', handleResponsiveCanvas);
+        return () => window.removeEventListener('resize', handleResponsiveCanvas);
+    },[layout])
         
     return(
         !isLoading ? 
--- src/features/shared/quickies.tsx
+++ src/features/shared/quickies.tsx
@@ -2,3 +2,5 @@ import dayjs from 'dayjs'
 import { CoreResponse } from './basicSanityTypes'
 
 export const postedDate = (entry: CoreResponse) => dayjs(entry._createdAt, "YYYY-MM-DDTHH:mm:ssZ").format("MMMM D YYYY").toLocaleLowerCase()
+
+export const returnLayout = () => window.innerHeight > window.innerWidth ? 'portrait' : 'landscape'
